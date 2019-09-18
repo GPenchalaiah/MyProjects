90b97e86f74ffcee5973a580383d94be5c2afe2e
lenovo <lenovo@192.168.0.41>
2019-09-18 21:18:16 +0530

    Update

---
+++ SpringCore/src/main/java/com/tcs/SpringCore/Company.java
@@ -0,0 +1,30 @@
+package com.tcs.SpringCore;
+
+public class Company {
+
+	private Integer cid;
+	private String cname;
+
+	public Integer getCid() {
+		return cid;
+	}
+
+	public void setCid(Integer cid) {
+		this.cid = cid;
+	}
+
+	public String getCname() {
+		return cname;
+	}
+
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+
+	@Override
+	public String toString() {
+		return "Company [cid=" + cid + ", cname=" + cname + "]";
+	}
+
+	
+}
